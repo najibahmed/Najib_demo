cea83c61c59a36082ca246ea86cf6019036be9cd
unknown <najibahmed50@gmail.com>
2021-09-19 21:13:56 +0600

    Personal Information class 8

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,47 +9,117 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.blue,
         title: Text(
-          "Facebook",
+          "Personal Information",
           style: TextStyle(
             color: Colors.white,
             fontSize: 32,
-            fontFamily: "arial",
+            fontFamily: "Lobster",
             fontWeight: FontWeight.w700,
           ),
         ),
       ),
-
-      // body: Center(
-      //   child: Icon(
-      //     Icons.add_a_photo_outlined,
-      //     size: 180,
-      //     color: Colors.blue,
-      //   ),
-      // ),
-
-      // body: Center(
-      //   child: Text(
-      //     "The quick brown fox jumps over  lazy dog.",
-      //     style: TextStyle(
-      //       fontFamily: "Lobster",
-      //       fontSize: 28,
-      //     ),
-      //   ),
-      // ),
-
-      // body: Image.asset(
-      //   "assets/happy_man.jpg",
-      //   height: 250,
-      //   width: 250,
-      // ),
-
-      body: Center(
-        child: CircleAvatar(
-          radius: 180,
-          backgroundColor: Colors.blue,
-          foregroundImage: AssetImage(
-            "assets/happy_man.jpg",
-          ),
+      body: Container(
+        color: Colors.indigo,
+        width: double.infinity,
+        height: double.infinity,
+        padding: EdgeInsets.all(30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // circuler image profile picture
+            CircleAvatar(
+              radius: 60,
+              foregroundImage: AssetImage("assets/happy_man.jpg"),
+            ),
+            const SizedBox(height: 20),
+            // text with name
+            Container(
+              child: Text(
+                "Najib Ahmed",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700,
+                  fontFamily: "Lobster",
+                  fontSize: 32,
+                  letterSpacing: 5.0,
+                ),
+              ),
+            ),
+            // text with developer
+            Container(
+              child: Text(
+                "Flutter Developer",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: "Playball",
+                  fontSize: 32,
+                  letterSpacing: 5.0,
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            // bar between objects
+            Container(
+              width: 200,
+              height: 1,
+              color: Colors.white,
+            ),
+            const SizedBox(height: 30),
+            // for phone number container
+            Container(
+              width: double.infinity,
+              height: 50,
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(children: [
+                Icon(
+                  Icons.call,
+                  size: 20,
+                  color: Colors.black,
+                ),
+                const SizedBox(width: 20),
+                Text(
+                  "+8801717696902",
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.black,
+                    letterSpacing: 3.0,
+                  ),
+                ),
+              ]),
+            ),
+            const SizedBox(height: 20),
+            // for emain adress
+            Container(
+              width: double.infinity,
+              height: 50,
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(children: [
+                Icon(
+                  Icons.mail,
+                  size: 20,
+                  color: Colors.black,
+                ),
+                const SizedBox(width: 20),
+                Text(
+                  "najibahmed50@gmail.com",
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.black,
+                    letterSpacing: 3.0,
+                  ),
+                ),
+              ]),
+            ),
+          ],
         ),
       ),
     );
