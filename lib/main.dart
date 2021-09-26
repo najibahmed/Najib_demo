14551007230d128b0d88576b2326639476aeb8e8
unknown <najibahmed50@gmail.com>
2021-09-26 21:02:13 +0600

    list view Contact list

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:najib_project/stack_positioned.dart';
+
+import 'list_view.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "demo_project",
-      //
-      home: Stackpage(),
-    ),
+        debugShowCheckedModeBanner: false,
+        title: ("demo_project"),
+
+        // home: Stackpage(),
+        home: Listview()),
   );
 }
